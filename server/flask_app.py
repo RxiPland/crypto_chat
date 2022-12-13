828edaf19c6709e34131d5ceed41a6e4e1d0f905
RxiPland <82058894+RxiPland@users.noreply.github.com>
2022-12-13 18:11:06 +0100

    Add files via upload

--- server/flask_app.py
+++ server/flask_app.py
@@ -1,3 +1,26 @@
-from flask import Flask, Request, request
+from flask import Flask, request
+from cryptography.fernet import Fernet
 
-app = Flask("crypt-chat server")
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+
+@app.route('/crypt/<zprava>', methods=["GET"])
+def sifrovat_zpravu(zprava: str):
+
+
+    key = Fernet.generate_key()
+    f = Fernet(key)
+
+    zasifrovano = f.encrypt(zprava.encode())
+
+    desifrovano = f.decrypt(zasifrovano)
+    
+
+    return "Původní zpráva: " + zprava + "<br><br>Vygenerovaný symetrický klíč: " + key.decode()  + "<br><br>Zašifrovaná zpráva: " + zasifrovano.decode() + "<br><br>Dešifrovaná zpráva: " + desifrovano.decode()
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
