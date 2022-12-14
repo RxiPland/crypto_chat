673dae1b1d9651a1419b91c13129f275e814f36e
RxiPland <82058894+RxiPland@users.noreply.github.com>
2022-12-14 21:20:39 +0100

    Add files via upload

--- crypt_chat/tools/cryptography_tool.py
+++ crypt_chat/tools/cryptography_tool.py
@@ -1,24 +1,26 @@
-from rsa import PublicKey, encrypt, newkeys
+import rsa
 from cryptography.fernet import Fernet
-from base64 import urlsafe_b64decode, urlsafe_b64encode
+import base64
 
 import sys
 
 def main():
 
-    # operations -> [generate_rsa, decrypt_rsa, decrypt_aes]
+    # operations -> [generate_rsa, decrypt_rsa, encrypt_aes, decrypt_aes]
     # arguments -> [key_size: int, b64_text: bytes, b64_text: bytes]
 
     argv = sys.argv[1:]
     length = len(argv)
     
     if length == 0:
-        print("Chybí operace! [generate_rsa, decrypt_rsa, decrypt_aes]")
+        print("Chybí operace! [generate_rsa, generate_aes ,decrypt_rsa, encrypt_aes, decrypt_aes]")
         return
     
     operation = argv[0].lower()
     
     if operation == "generate_rsa":
+        # generate RSA key pairs & save to files
+
 
         # check if there is second argument
         if length == 2:
@@ -34,8 +36,7 @@ def main():
 
             else:
                 # generate keys
-
-                public_k, private_k = newkeys(int(argv[1]))
+                public_k, private_k = rsa.newkeys(int(argv[1]))
 
                 # save to file
                 with open("public_key.pem", "wb") as f:
@@ -48,9 +49,67 @@ def main():
         else:
             print("Chybí velikost RSA klíče v bitech! [2048, 4096]")
             return
+
+    elif operation == "generate_aes":
+
+        symetric_key = Fernet.generate_key()
+        
+        with open("symetric_key", "wb") as f:
+            f.write(symetric_key)
+        
+
+    elif operation == "decrypt_rsa":
+        # decrypt RSA to obtain symetric key & save it to file
+
+        if length == 2:
+            
+            message_crypt = base64.urlsafe_b64decode(argv[1].encode("utf-8"))
+
+            with open("private_key.pem", "rb") as f:
+                private_k = rsa.PrivateKey.load_pkcs1(f.read())
+
+            message_plain = rsa.decrypt(crypto=message_crypt, priv_key=private_k)
+
+            with open("symetric_key", "wb") as f:
+                f.write(message_plain)
+
+
+    elif operation == "encrypt_aes":
+        # encrypt user's "base64 text" to encrypted_message file
+
+        if length == 2:
+        
+            message_plain = argv[1].encode("utf-8")
+
+            with open("symetric_key", "rb") as f:
+                symetric_key = Fernet(f.read())
+
+            message_crypt = symetric_key.encrypt(data=message_plain)
+
+            with open("encrypted_message", "wb") as f:
+                f.write(message_crypt)
+
+        else:
+            print("Chybí base64 text!")
+            return
+
+    elif operation == "decrypt_aes":
+        # open encrypted_message file & decrypt to decrypted_message file
+
+        with open("encrypted_message", "rb") as f:
+            message_crypt = f.read()
+
+        with open("symetric_key", "rb") as f:
+            symetric_key = Fernet(f.read())
+
+        message_plain = symetric_key.decrypt(message_crypt)
+
+        with open("decrypted_message", "wb") as f:
+            f.write(base64.urlsafe_b64decode(message_plain))
+
     
     else:
-        print("Neplatná operace! [generate_rsa, decrypt_rsa, decrypt_aes]")
+        print("Neplatná operace! [generate_rsa, generate_aes, decrypt_rsa, encrypt_aes, decrypt_aes]")
         return
     
 
